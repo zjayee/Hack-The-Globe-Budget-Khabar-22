d1f19f632bed35599593bbf6ee3df4e57e35ab4a
ZJayee <jayee.zheng@gmail.com>
2023-03-19 02:40:23 -0400

    add nutrient summary

--- app/src/main/java/com/example/myapplication/FoodChoiceFragment.kt
+++ app/src/main/java/com/example/myapplication/FoodChoiceFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -38,6 +39,43 @@ class FoodChoiceFragment : Fragment() {
         nextFab.setOnClickListener {
             findNavController().navigate(R.id.action_foodChoiceFragment_to_nutritionalValueFragment)
         }
+
+        val bananaCard = rootView.findViewById<View>(R.id.banana)
+        val pineappleCard = rootView.findViewById<View>(R.id.pineapple)
+        val carrotCard = rootView.findViewById<View>(R.id.carrot)
+        val tomatoCard = rootView.findViewById<View>(R.id.tomato)
+        val cabbageCard = rootView.findViewById<View>(R.id.cabbage)
+        val eggCard = rootView.findViewById<View>(R.id.eggs)
+
+        bananaCard.findViewById<TextView>(R.id.food_name_textview).text = "Banana"
+        bananaCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳35"
+        bananaCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳8.75"
+        bananaCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 4"
+
+        pineappleCard.findViewById<TextView>(R.id.food_name_textview).text = "Pineapple"
+        pineappleCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳53"
+        pineappleCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳13.25"
+        pineappleCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 4"
+
+        carrotCard.findViewById<TextView>(R.id.food_name_textview).text = "Carrot"
+        carrotCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳17"
+        carrotCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳2.83"
+        carrotCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 2"
+
+        tomatoCard.findViewById<TextView>(R.id.food_name_textview).text = "Tomato"
+        tomatoCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳17"
+        tomatoCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳4.25"
+        tomatoCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 4"
+
+        cabbageCard.findViewById<TextView>(R.id.food_name_textview).text = "Cabbage"
+        cabbageCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳35"
+        cabbageCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳17.5"
+        cabbageCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 8"
+
+        eggCard.findViewById<TextView>(R.id.food_name_textview).text = "Eggs"
+        eggCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳62"
+        eggCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳10.33"
+        eggCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 2"
 
         return rootView
     }
--- app/src/main/java/com/example/myapplication/NutritionalValueFragment.kt
+++ app/src/main/java/com/example/myapplication/NutritionalValueFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +19,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NutritionalValueFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var rootView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,26 +34,50 @@ class NutritionalValueFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutritional_value, container, false)
+        rootView = inflater.inflate(R.layout.fragment_nutritional_value, container, false)
+
+        val bananaCard = rootView.findViewById<View>(R.id.banana)
+        val carrotCard = rootView.findViewById<View>(R.id.carrot)
+        val eggCard = rootView.findViewById<View>(R.id.eggs)
+
+        bananaCard.findViewById<TextView>(R.id.food_name_textview).text = "Banana"
+        bananaCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳35"
+        bananaCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳8.75"
+        bananaCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 4"
+        bananaCard.findViewById<CheckBox>(R.id.checkbox).visibility = View.GONE
+
+        carrotCard.findViewById<TextView>(R.id.food_name_textview).text = "Carrot"
+        carrotCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳17"
+        carrotCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳2.83"
+        carrotCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 2"
+        carrotCard.findViewById<CheckBox>(R.id.checkbox).visibility = View.GONE
+
+        eggCard.findViewById<TextView>(R.id.food_name_textview).text = "Eggs"
+        eggCard.findViewById<TextView>(R.id.price_textview).text = "Price: ৳62"
+        eggCard.findViewById<TextView>(R.id.price_meal_textview).text = "Price/Meal: ৳10.33"
+        eggCard.findViewById<TextView>(R.id.rec_num_textview).text = "Rec #: 2"
+        eggCard.findViewById<CheckBox>(R.id.checkbox).visibility = View.GONE
+
+        return rootView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NutritionalValueFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NutritionalValueFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment NutritionalValueFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            NutritionalValueFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
